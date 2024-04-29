5d4e5730ef9b0868764cbb6e2e9b1dd70edbfc2e
abbia77 <superlorenzo16@gmail.com>
2024-04-29 10:54:53 +0200

    sdd

---
+++ database.js
@@ -0,0 +1,17 @@
+    const mysql = require('mysql2');
+    const { HOST, USERNAME, PASSWORD, smartphone2, PORT } = process.env;
+    console.log();
+    const connpool = mysql.createPool({
+        host: "10.20.2.173",
+        user: "tps",
+        password: "ttppss",
+        database: "td"
+    }, { debug: true });
+    
+    connpool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        if (error) throw error;
+        console.log('Db is connected - The solution is: ', results[0].solution);
+    });
+    
+    
+    module.exports = connpool;
